ce1e2f31d2640f9667f67ede5c503cfc8c4351e9
Ni Nuttakit <p.nuttakit@tqd.co.th>
2025-08-12 11:41:17 +0700

    clean code

--- src/Core/Interfaces/IEmailService.cs
+++ src/Core/Interfaces/IEmailService.cs
@@ -5,7 +5,5 @@ namespace Core.Interfaces;
 
 public interface IEmailService
 {
-    Task<bool> SendAlertEmailAsync(string toEmail, string subject, string message, int alertId);
-    Task<bool> SendBulkAlertEmailAsync(IEnumerable<string> toEmails, string subject, string message, int alertId);
     Task SendDisasterAlertEmailAsync(User user, DisasterRiskReportResponse report, string regionName);
 }
